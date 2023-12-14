212d8151521018762f3cdb4172fa08d040e43a05
Amin Jamal <a.jamal@greyrook.com>
2023-12-15 00:10:06 +0100

    feat: add constructor to the Delta type

--- src/delta.rs
+++ src/delta.rs
@@ -1,3 +1,4 @@
+use crate::utils::calculate_hash;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::hash::{Hash, Hasher};
@@ -32,11 +33,41 @@ pub struct Delta {
     pub hash: String,
 }
 
+impl Delta {
+    pub fn new(operation: Operation, path: String, old_value: Value, new_value: Value) -> Self {
+        let mut delta = Delta {
+            operation,
+            path,
+            old_value,
+            new_value,
+            hash: String::default(),
+        };
+        delta.hash = calculate_hash(&delta).to_string();
+        delta
+    }
+}
+
 impl Hash for Delta {
     fn hash<H: Hasher>(&self, state: &mut H) {
         self.operation.hash(state);
         self.path.hash(state);
         self.old_value.to_string().hash(state);
         self.new_value.to_string().hash(state);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_delta_hash() {
+        let delta = Delta::new(
+            Operation::Add,
+            "$.test".to_string(),
+            Value::Null,
+            Value::Number(1.into()),
+        );
+        assert_eq!(delta.hash, "13989947290824433245");
     }
 }
